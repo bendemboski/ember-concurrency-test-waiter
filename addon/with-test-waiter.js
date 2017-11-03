4bf4100b46390e9264b0f21d34e1e2afb76c08c3
Ben Demboski <ben@turbopatent.com>
2017-11-03 15:05:11 -0700

    Update to ember-cli 2.16.2
    Mainly to get an updated babel so we don't force a regenerator runtime on apps consuming this addon.

--- addon/with-test-waiter.js
+++ addon/with-test-waiter.js
@@ -1,34 +1,30 @@
+import { assert } from '@ember/debug';
+import { registerWaiter } from '@ember/test';
 import Ember from 'ember';
 
-const {
-  Test,
-  assert,
-  testing
-} = Ember;
-
+let registered = false;
 let taskRunCounter = 0;
-
-if (testing) {
-  Test.registerWaiter(() => taskRunCounter === 0);
-}
 
 // A function that, given a task property, will register it with the test
 // waiter so async test helpers will block anytime a task instance spawned
 // from that property is running.
 export default function withTestWaiter(taskProperty) {
-  if (testing) {
-    assert("withTestWaiter() will only work with ember-concurrency >=0.7.19 -- please upgrade", taskProperty.taskFn);
+  assert("withTestWaiter() will only work with ember-concurrency >=0.7.19 -- please upgrade", taskProperty.taskFn);
 
-    let originalTaskFn = taskProperty.taskFn;
+  let originalTaskFn = taskProperty.taskFn;
 
-    taskProperty.taskFn = function *(...args) {
-      taskRunCounter += 1;
-      try {
-        return yield * originalTaskFn.apply(this, args);
-      } finally {
-        taskRunCounter -= 1;
-      }
-    };
-  }
+  taskProperty.taskFn = function *(...args) {
+    if (Ember.testing && !registered) {
+      registerWaiter(() => taskRunCounter === 0);
+      registered = true;
+    }
+
+    taskRunCounter += 1;
+    try {
+      return yield * originalTaskFn.apply(this, args);
+    } finally {
+      taskRunCounter -= 1;
+    }
+  };
   return taskProperty;
 }
