599363cad60ce0d593523807c17d9051d12e3c39
Ben Demboski <ben@turbopatent.com>
2018-12-03 10:48:21 -0800

    Update to nested QUnit modules

--- tests/unit/define-modifier-test.js
+++ tests/unit/define-modifier-test.js
@@ -7,35 +7,35 @@ import { TaskProperty } from 'ember-concurrency/-task-property';
 import { module, test } from 'qunit';
 import checkWaiters from '../helpers/check-waiters';
 
-module('Unit | define modifier', {
-  afterEach() {
+module('Unit | define modifier', function(hooks) {
+  hooks.afterEach(function() {
     delete TaskProperty.prototype.withTestWaiter;
-  }
-});
-
-test('it works', function(assert) {
-  defineModifier();
-
-  let Obj = EmberObject.extend({
-    doStuff: task(function * () {
-      return yield new EmberPromise((resolve) => {
-        this.resolvePromise = resolve;
-      });
-    }).withTestWaiter(),
   });
 
-  let obj;
+  test('it works', function(assert) {
+    defineModifier();
 
-  run(() => {
-    obj = Obj.create();
-    obj.get('doStuff').perform();
+    let Obj = EmberObject.extend({
+      doStuff: task(function * () {
+        return yield new EmberPromise((resolve) => {
+          this.resolvePromise = resolve;
+        });
+      }).withTestWaiter(),
+    });
+
+    let obj;
+
+    run(() => {
+      obj = Obj.create();
+      obj.get('doStuff').perform();
+    });
+
+    assert.ok(checkWaiters(), "waiters are not settled while task is running");
+
+    run(() => {
+      obj.resolvePromise();
+    });
+
+    assert.notOk(checkWaiters(), "waiters are settled after task completes");
   });
-
-  assert.ok(checkWaiters(), "waiters are not settled while task is running");
-
-  run(() => {
-    obj.resolvePromise();
-  });
-
-  assert.notOk(checkWaiters(), "waiters are settled after task completes");
 });
