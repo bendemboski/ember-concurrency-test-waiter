ee807bfc035725c95c75ba9c600aedbe3b8ebbe8
Steve Calvert <scalvert@linkedin.com>
2020-04-13 15:25:38 -0700

    Fixing borked single quotes

--- tests/unit/with-test-waiter-test.js
+++ tests/unit/with-test-waiter-test.js
@@ -1,13 +1,13 @@
-import EmberObject from "@ember/object";
-import { Promise } from "rsvp";
-import { run } from "@ember/runloop";
-import withTestWaiter from "ember-concurrency-test-waiter/with-test-waiter";
-import { task } from "ember-concurrency";
-import { module, test } from "qunit";
+import EmberObject from '@ember/object';
+import { Promise } from 'rsvp';
+import { run } from '@ember/runloop';
+import withTestWaiter from 'ember-concurrency-test-waiter/with-test-waiter';
+import { task } from 'ember-concurrency';
+import { module, test } from 'qunit';
 import { hasPendingWaiters } from 'ember-test-waiters';
  
-module("Unit | with test waiter", function () {
-  test("no test waiter is registered without withTestWaiter() decorator", function (assert) {
+module('Unit | with test waiter', function () {
+  test('no test waiter is registered without withTestWaiter() decorator', function (assert) {
     let Obj = EmberObject.extend({
       doStuff: task(function* () {
         return yield new Promise(() => null);
@@ -18,13 +18,13 @@ module("Unit | with test waiter", function () {
 
     run(() => {
       obj = Obj.create();
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
     assert.notOk(hasPendingWaiters());
   });
 
-  test("withTestWaiter() decorator works", function (assert) {
+  test('withTestWaiter() decorator works', function (assert) {
     let Obj = EmberObject.extend({
       doStuff: withTestWaiter(
         task(function* () {
@@ -39,19 +39,19 @@ module("Unit | with test waiter", function () {
 
     run(() => {
       obj = Obj.create();
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
-    assert.ok(hasPendingWaiters(), "waiters are not settled while task is running");
+    assert.ok(hasPendingWaiters(), 'waiters are not settled while task is running');
 
     run(() => {
       obj.resolvePromise();
     });
 
-    assert.notOk(hasPendingWaiters(), "waiters are settled after task completes");
+    assert.notOk(hasPendingWaiters(), 'waiters are settled after task completes');
   });
 
-  test("withTestWaiter() decorator works with concurrent tasks", function (assert) {
+  test('withTestWaiter() decorator works with concurrent tasks', function (assert) {
     let Obj = EmberObject.extend({
       doStuff: withTestWaiter(
         task(function* () {
@@ -67,18 +67,18 @@ module("Unit | with test waiter", function () {
 
     run(() => {
       obj = Obj.create();
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
-    assert.ok(hasPendingWaiters(), "waiters are not settled while task is running");
+    assert.ok(hasPendingWaiters(), 'waiters are not settled while task is running');
 
     run(() => {
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
     assert.ok(
       hasPendingWaiters(),
-      "waiters are not settled while two concurrent tasks are running"
+      'waiters are not settled while two concurrent tasks are running'
     );
 
     run(() => {
@@ -87,7 +87,7 @@ module("Unit | with test waiter", function () {
 
     assert.ok(
       hasPendingWaiters(),
-      "waiters are not settled after only one task completes"
+      'waiters are not settled after only one task completes'
     );
 
     run(() => {
@@ -96,11 +96,11 @@ module("Unit | with test waiter", function () {
 
     assert.notOk(
       hasPendingWaiters(),
-      "waiters are settled after both tasks complete"
+      'waiters are settled after both tasks complete'
     );
   });
 
-  test("withTestWaiter() decorator works with restartable()", function (assert) {
+  test('withTestWaiter() decorator works with restartable()', function (assert) {
     let Obj = EmberObject.extend({
       doStuff: withTestWaiter(
         task(function* () {
@@ -115,25 +115,25 @@ module("Unit | with test waiter", function () {
 
     run(() => {
       obj = Obj.create();
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
-    assert.ok(hasPendingWaiters(), "waiters are not settled while task is running");
+    assert.ok(hasPendingWaiters(), 'waiters are not settled while task is running');
 
     run(() => {
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
-    assert.ok(hasPendingWaiters(), "waiters are not settled after restarting task");
+    assert.ok(hasPendingWaiters(), 'waiters are not settled after restarting task');
 
     run(() => {
       obj.resolvePromise();
     });
 
-    assert.notOk(hasPendingWaiters(), "waiters are settled after task completes");
+    assert.notOk(hasPendingWaiters(), 'waiters are settled after task completes');
   });
 
-  test("withTestWaiter() decorator works with enqueue()", function (assert) {
+  test('withTestWaiter() decorator works with enqueue()', function (assert) {
     let Obj = EmberObject.extend({
       doStuff: withTestWaiter(
         task(function* () {
@@ -149,16 +149,16 @@ module("Unit | with test waiter", function () {
 
     run(() => {
       obj = Obj.create();
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
-    assert.ok(hasPendingWaiters(), "waiters are not settled while task is running");
+    assert.ok(hasPendingWaiters(), 'waiters are not settled while task is running');
 
     run(() => {
-      obj.get("doStuff").perform();
+      obj.get('doStuff').perform();
     });
 
-    assert.ok(hasPendingWaiters(), "waiters are not settled while task is queued");
+    assert.ok(hasPendingWaiters(), 'waiters are not settled while task is queued');
 
     run(() => {
       obj.resolvePromises[0]();
@@ -166,7 +166,7 @@ module("Unit | with test waiter", function () {
 
     assert.ok(
       hasPendingWaiters(),
-      "waiters are not settled after only one task completes"
+      'waiters are not settled after only one task completes'
     );
 
     run(() => {
@@ -175,7 +175,7 @@ module("Unit | with test waiter", function () {
 
     assert.notOk(
       hasPendingWaiters(),
-      "waiters are settled after both tasks complete"
+      'waiters are settled after both tasks complete'
     );
   });
 });
